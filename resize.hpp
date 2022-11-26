d81e2b60891d9b8585d4e7713a52b8f1f5defda0
acgzone <acgzone@163.com>
2022-11-26 23:35:37 +0800

    enable avx512

--- resize.hpp
+++ resize.hpp
@@ -54,8 +54,8 @@ static void BGRAfterBiCubic(RGBImage src, float x_float, float y_float, unsigned
   int x0 = floor(x_float) - 1;
   int y0 = floor(y_float) - 1;
 
-  float sumf[3] = {.0f};
-//  #pragma simd
+  /*float sumf[3] = {.0f};
+  //#pragma simd
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       float temp = coeff[i * 4 + j];
@@ -64,7 +64,8 @@ static void BGRAfterBiCubic(RGBImage src, float x_float, float y_float, unsigned
       sumf[1] += temp * src.data[position + 1];
       sumf[2] += temp * src.data[position + 2];
     }
-  }
+  }*/
+
   /*__m128 row_0_coeff = _mm_load_ss(coeff);
   __m128 row_1_coeff = _mm_load_ss(coeff+4);
   __m128 row_2_coeff = _mm_load_ss(coeff+8);
@@ -85,7 +86,20 @@ static void BGRAfterBiCubic(RGBImage src, float x_float, float y_float, unsigned
   __m128 row_3_data_0 = _mm_set_ps((float)src.data[(row3 + 3) * channels + 0],(float)src.data[(row3 + 2) * channels + 0],(float)src.data[(row2 + 1) * channels + 0],(float)src.data[(row3 + 0) * channels + 0]);
   __m128 row_3_data_1 = _mm_set_ps((float)src.data[(row3 + 3) * channels + 1],(float)src.data[(row3 + 2) * channels + 1],(float)src.data[(row2 + 1) * channels + 1],(float)src.data[(row3 + 0) * channels + 1]);
   __m128 row_3_data_2 = _mm_set_ps((float)src.data[(row3 + 3) * channels + 2],(float)src.data[(row3 + 2) * channels + 2],(float)src.data[(row2 + 1) * channels + 2],(float)src.data[(row3 + 0) * channels + 2]);  
-  //_mm_mul_ss
+  */
+  
+  __m512 __coeff = _mm512_load_ps((void *)coeff);
+
+  int row0 = (x0 + 0) * src.cols + y0 ;
+  int row1 = (x0 + 1) * src.cols + y0 ;
+  int row2 = (x0 + 2) * src.cols + y0 ;
+  int row3 = (x0 + 3) * src.cols + y0 ;
+
+  __m512 __data_0 = _mm512_set_ps((float)src.data[(row3 + 3) * channels + 0],(float)src.data[(row3 + 2) * channels + 0],(float)src.data[(row2 + 1) * channels + 0],(float)src.data[(row3 + 0) * channels + 0],(float)src.data[(row2 + 3) * channels + 0],(float)src.data[(row2 + 2) * channels + 0],(float)src.data[(row2 + 1) * channels + 0],(float)src.data[(row2 + 0) * channels + 0],(float)src.data[(row1 + 3) * channels + 0],(float)src.data[(row1 + 2) * channels + 0],(float)src.data[(row1 + 1) * channels + 0],(float)src.data[(row1 + 0) * channels + 0],(float)src.data[(row0 + 3) * channels + 0],(float)src.data[(row0 + 2) * channels + 0],(float)src.data[(row0 + 1) * channels + 0],(float)src.data[(row0 + 0) * channels + 0]);
+  __m512 __data_1 = _mm512_set_ps((float)src.data[(row3 + 3) * channels + 1],(float)src.data[(row3 + 2) * channels + 1],(float)src.data[(row2 + 1) * channels + 1],(float)src.data[(row3 + 0) * channels + 1],(float)src.data[(row2 + 3) * channels + 1],(float)src.data[(row2 + 2) * channels + 1],(float)src.data[(row2 + 1) * channels + 1],(float)src.data[(row2 + 0) * channels + 1],(float)src.data[(row1 + 3) * channels + 1],(float)src.data[(row1 + 2) * channels + 1],(float)src.data[(row1 + 1) * channels + 1],(float)src.data[(row1 + 0) * channels + 1],(float)src.data[(row0 + 3) * channels + 1],(float)src.data[(row0 + 2) * channels + 1],(float)src.data[(row0 + 1) * channels + 1],(float)src.data[(row0 + 0) * channels + 1]);
+  __m512 __data_2 = _mm512_set_ps((float)src.data[(row3 + 3) * channels + 2],(float)src.data[(row3 + 2) * channels + 2],(float)src.data[(row2 + 1) * channels + 2],(float)src.data[(row3 + 0) * channels + 2],(float)src.data[(row2 + 3) * channels + 2],(float)src.data[(row2 + 2) * channels + 2],(float)src.data[(row2 + 1) * channels + 2],(float)src.data[(row2 + 0) * channels + 2],(float)src.data[(row1 + 3) * channels + 2],(float)src.data[(row1 + 2) * channels + 2],(float)src.data[(row1 + 1) * channels + 2],(float)src.data[(row1 + 0) * channels + 2],(float)src.data[(row0 + 3) * channels + 2],(float)src.data[(row0 + 2) * channels + 2],(float)src.data[(row0 + 1) * channels + 2],(float)src.data[(row0 + 0) * channels + 2]);
+  
+  /*//_mm_mul_ss
   __m128 row_0_sum_0 = _mm_mul_ss(row_0_coeff, row_0_data_0);
   __m128 row_0_sum_1 = _mm_mul_ss(row_0_coeff, row_0_data_1);
   __m128 row_0_sum_2 = _mm_mul_ss(row_0_coeff, row_0_data_2);
@@ -97,14 +111,23 @@ static void BGRAfterBiCubic(RGBImage src, float x_float, float y_float, unsigned
   __m128 row_2_sum_2 = _mm_mul_ss(row_2_coeff, row_2_data_2);
   __m128 row_3_sum_0 = _mm_mul_ss(row_3_coeff, row_3_data_0);
   __m128 row_3_sum_1 = _mm_mul_ss(row_3_coeff, row_3_data_1);
-  __m128 row_3_sum_2 = _mm_mul_ss(row_3_coeff, row_3_data_2);
+  __m128 row_3_sum_2 = _mm_mul_ss(row_3_coeff, row_3_data_2);*/
+  //_mm512_mul_ps
 
-  sum[0] = static_cast<unsigned char>(row_0_sum_0[0]+row_0_sum_0[1]+row_0_sum_0[2]+row_0_sum_0[3]+row_1_sum_0[0]+row_1_sum_0[1]+row_1_sum_0[2]+row_1_sum_0[3]+row_2_sum_0[0]+row_2_sum_0[1]+row_2_sum_0[2]+row_2_sum_0[3]+row_3_sum_0[0]+row_3_sum_0[1]+row_3_sum_0[2]+row_3_sum_0[3]);
+  __m512 __sum_0 = _mm512_mul_ps(__coeff,__data_0);
+  __m512 __sum_1 = _mm512_mul_ps(__coeff,__data_1);
+  __m512 __sum_2 = _mm512_mul_ps(__coeff,__data_2);
+
+  /*sum[0] = static_cast<unsigned char>(row_0_sum_0[0]+row_0_sum_0[1]+row_0_sum_0[2]+row_0_sum_0[3]+row_1_sum_0[0]+row_1_sum_0[1]+row_1_sum_0[2]+row_1_sum_0[3]+row_2_sum_0[0]+row_2_sum_0[1]+row_2_sum_0[2]+row_2_sum_0[3]+row_3_sum_0[0]+row_3_sum_0[1]+row_3_sum_0[2]+row_3_sum_0[3]);
   sum[1] = static_cast<unsigned char>(row_0_sum_1[0]+row_0_sum_1[1]+row_0_sum_1[2]+row_0_sum_1[3]+row_1_sum_1[0]+row_1_sum_1[1]+row_1_sum_1[2]+row_1_sum_1[3]+row_2_sum_1[0]+row_2_sum_1[1]+row_2_sum_1[2]+row_2_sum_1[3]+row_3_sum_1[0]+row_3_sum_1[1]+row_3_sum_1[2]+row_3_sum_1[3]);
-  sum[2] = static_cast<unsigned char>(row_0_sum_2[0]+row_0_sum_2[1]+row_0_sum_2[2]+row_0_sum_2[3]+row_1_sum_2[0]+row_1_sum_2[1]+row_1_sum_2[2]+row_1_sum_2[3]+row_2_sum_2[0]+row_2_sum_2[1]+row_2_sum_2[2]+row_2_sum_2[3]+row_3_sum_2[0]+row_3_sum_2[1]+row_3_sum_2[2]+row_3_sum_2[3]);*/  
-  sum[0] = static_cast<unsigned char>(sumf[0]);
+  sum[2] = static_cast<unsigned char>(row_0_sum_2[0]+row_0_sum_2[1]+row_0_sum_2[2]+row_0_sum_2[3]+row_1_sum_2[0]+row_1_sum_2[1]+row_1_sum_2[2]+row_1_sum_2[3]+row_2_sum_2[0]+row_2_sum_2[1]+row_2_sum_2[2]+row_2_sum_2[3]+row_3_sum_2[0]+row_3_sum_2[1]+row_3_sum_2[2]+row_3_sum_2[3]);*/
+  /*sum[0] = static_cast<unsigned char>(sumf[0]);
   sum[1] = static_cast<unsigned char>(sumf[1]);
-  sum[2] = static_cast<unsigned char>(sumf[2]); 
+  sum[2] = static_cast<unsigned char>(sumf[2]); */
+
+  sum[0] = static_cast<unsigned char>(__sum_0[0] + __sum_0[1] +__sum_0[2] +__sum_0[3] +__sum_0[4] +__sum_0[5] +__sum_0[6] +__sum_0[7] +__sum_0[8] +__sum_0[9] +__sum_0[10] +__sum_0[11] +__sum_0[12] +__sum_0[13] +__sum_0[14] +__sum_0[15]);
+  sum[1] = static_cast<unsigned char>(__sum_1[0] + __sum_1[1] +__sum_1[2] +__sum_1[3] +__sum_1[4] +__sum_1[5] +__sum_1[6] +__sum_1[7] +__sum_1[8] +__sum_1[9] +__sum_1[10] +__sum_1[11] +__sum_1[12] +__sum_1[13] +__sum_1[14] +__sum_1[15]);
+  sum[2] = static_cast<unsigned char>(__sum_2[0] + __sum_2[1] +__sum_2[2] +__sum_2[3] +__sum_2[4] +__sum_2[5] +__sum_2[6] +__sum_2[7] +__sum_2[8] +__sum_2[9] +__sum_2[10] +__sum_2[11] +__sum_2[12] +__sum_2[13] +__sum_2[14] +__sum_2[15]);
   return ;
 }
 
@@ -122,7 +145,7 @@ void ResizeImagePart(RGBImage src, float ratio, int x_left, int x_right, int y_u
   auto check_perimeter = [src](float x, float y) -> bool {
     return x < src.rows - 2 && x > 1 && y < src.cols - 2 && y > 1;
   };
-  Timer part("Worker");
+//  Timer part("Worker");
   float coeff[16];
   unsigned char sum[3];
   for (int i = x_left; i < x_right; i++) {
